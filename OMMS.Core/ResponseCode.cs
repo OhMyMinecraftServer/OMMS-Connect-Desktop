e1ebafab37842b29d0ad7a3e5641f9a7b082e29c
ZhuRuoLing <jkl-963@live.com>
2023-02-06 14:48:05 +0800

    Add enum class ResponseCode

---
+++ OMMS.Core/ResponseCode.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OMMS.Core;
+
+enum ResponseCode
+{
+    FAIL,
+    OK,
+    PERMISSION_DENIED,
+    OPERATION_ALREADY_EXISTS,
+    CONTROLLER_NOT_EXIST,
+    CONTROLLER_NO_STATUS,
+    NO_WHITELIST,
+    WHITELIST_NOT_EXIST, RUNNER_NOT_EXIST,
+    NO_SUCH_PLAYER,
+    PLAYER_ALREADY_EXISTS,
+    VERSION_NOT_MATCH,
+    ANNOUNCEMENT_NOT_EXIST,
+    INVALID_ARGUMENTS,
+    NO_RUNNER, UNDEFINED
+}
